b5dbb6291d4ac09f96d5b4acdb1330020dfed3a7
Vadim Basko <vadim.p.basko@gmail.com>
2021-12-02 23:51:03 +0400

    jmix custom generator - add chart on screen

--- src/generators/react-typescript/charts/template/Component.tsx
+++ src/generators/react-typescript/charts/template/Component.tsx
@@ -1,11 +1,51 @@
 import React from "react";
-import {registerScreen} from "@haulmont/jmix-react-web";
+import { registerScreen } from "@haulmont/jmix-react-web";
+import { Bar } from '@nivo/bar'
 
 const ROUTING_PATH = '/<%= nameLiteral %>';
+
+const data = [
+  {
+    day: "Monday",
+    degress: 59
+  },
+  {
+    day: "Tuesday",
+    degress: 61
+  },
+  {
+    day: "Wednesday",
+    degress: 55
+  },
+  {
+    day: "Thursday",
+    degress: 78
+  },
+  {
+    day: "Friday",
+    degress: 71
+  },
+  {
+    day: "Saturday",
+    degress: 56
+  },
+  {
+    day: "Sunday",
+    degress: 67
+  }
+];
 
 const <%= className %> = () => (
   <div>
     <%= className %>
+    <Bar
+      width={600}
+      height={400}
+      data={data}
+      keys={["degress"]}
+      indexBy="day"
+      margin={{top: 50, right: 130, bottom: 50, left: 60}}
+    />
   </div>
 );
 
